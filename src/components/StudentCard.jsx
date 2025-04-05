400a5873da430e864ccc37ba3c0a3701592317a3
Arpitakharde <arpitakharde30@gmail.com>
2025-04-04 20:32:59 -0800

    initial commit

---
+++ src/components/StudentCard.jsx
@@ -0,0 +1,46 @@
+
+import React from "react";
+import DeleteIcon from "./../assets/delete.png"
+import EditIcon from "./../assets/edit.png"
+import axios from "axios";
+import toast, {Toaster} from "react-hot-toast" ;
+import { Link } from "react-router";
+
+function StudentCard({id, name, city, loadStudents}){
+
+    const deleteStudent = async() =>{
+        const response = await axios.delete(`http://localhost:5001/students/${id}`)
+        if(response.data.success){
+            toast.success(response.data.message)
+            loadStudents()
+        }
+        else{
+            toast.error(response.data.message)
+        }
+    }
+
+    return(
+        <div className="border-2 border-gray-300 p-4 my-4 rounded-md shadow-md relative"> 
+            <h2 className="text-2xl">
+                {id} - {name}
+            </h2>
+            <p>{city}</p>
+    
+            <img src={DeleteIcon}
+             alt="DeleteIcon" 
+             className="h[30px] cursor-pointer absolute top-1 right-5"
+             onClick={deleteStudent}
+             />
+    
+            <Link to={`/Edit/${id}`}>
+                <img src={EditIcon}
+                alt="EditIcon" 
+                className="h[20px] cursor-pointer absolute bottom-2 right-5"
+                />
+            </Link> 
+    
+             <Toaster/>
+        </div>
+        );
+    }
+export default StudentCard;
---
+++ src/views/Home.jsx
@@ -0,0 +1,46 @@
+
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router";
+import StudentCard from "./../components/StudentCard";
+import AddIcon from "./../assets/add-user.png";
+
+const Home = () => {
+  const [students, setStudents] = useState([]);
+
+  const loadStudents = async() => {
+    const response = await axios.get("http://localhost:5001/students")
+    setStudents(response.data.data)
+  }
+
+  useEffect(()=>{
+    loadStudents();
+  }, []);
+
+  return (
+    <div>
+      <h1 className='text-center text-6xl my-4'>All Students</h1>
+      <div>
+        {students.map((studObj, i) => {
+          const {id, name, city} = studObj;
+          return (
+            <StudentCard key={i} id={id} name={name} city={city} loadStudents={loadStudents}/>
+          )
+        })}
+      </div>
+
+      <Link to="./Add">
+        <img 
+          src={AddIcon} 
+          alt="Add User" 
+          className="h-10 fixed botton-5 right-5  top-130 cursor-pointer" 
+        />
+      </Link>
+      
+    </div>
+  )
+}
+
+export default Home;
+
+
